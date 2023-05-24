10295cfdc3bd16d9dc0429b18ee0c49b4af4db08
Fernando <fernando.bragga@gmail.com>
2023-05-24 21:53:38 +1000

    added jit to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,svelte,js,ts,}",
-    "./electron/**/*.{html,svelte,js,ts,}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    mode: "jit",
+    content: [
+        "./index.html",
+        "./src/**/*.{html,svelte,js,ts,}",
+        "./electron/**/*.{html,svelte,js,ts,}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
